faab9cd3fa4f1931ae8f6125243fae300afd1186
Tanjil <tanjilkh@gmail.com>
2024-06-20 20:52:11 +0100

    Update app.py
    Setup pymongo e flask-login

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+from flask_login import LoginManager, UserMixin, login_user, current_user, login_required, logout_user
 
 app = Flask(__name__)
+mongo = PyMongo(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # /ping - Verificar se está a funcionar
 @app.route("/ping")
